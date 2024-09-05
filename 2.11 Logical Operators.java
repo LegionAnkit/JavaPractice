2ce3ec91155c13b4581ec2c801d3fcfb796aa5bf
LEGION <ankitpandey1206@gmail.com>
2024-09-05 17:25:12 +0530

    Update 2.11 Logical Operators.java
    added unary operator with explanation

--- 2.11 Logical Operators.java
+++ 2.11 Logical Operators.java
@@ -5,7 +5,10 @@ public class hello {
 		int y=5;
 		int a=5;
 		int b=9;
-		
+		int k=10;
+		int t=-10
+		System.out.println(~k);//-11 (minus of total positive value which starts from 0)  
+		System.out.println(~t);//9 (positive of total minus, positive starts from 0)  
 //		boolean result= x>y && a<b ;
 //		boolean result= x>y || a<b ;
 //		boolean result= x>y && a>b ;
